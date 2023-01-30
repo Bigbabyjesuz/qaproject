4132d1cc16a8bb099b6308f1d2997486e55b3cab
Bigbabyjesuz <108031532+Bigbabyjesuz@users.noreply.github.com>
2023-01-30 11:33:39 +0000

    Add files via upload

---
+++ qaprojects.py
@@ -0,0 +1,6 @@
+import mysql.connector
+
+input("What is your name?")
+ict_score = input("What was your ICT test score? /100")
+maths_score = input("What was your Maths test score? /100")
+chemistry_score = input("What was your Chemistry test score? name /100")
