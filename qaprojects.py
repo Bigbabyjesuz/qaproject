2914f7a6fd5a9c713b0f4479aa4857041ae06bb1
Bigbabyjesuz <c.broomer@outlook.com>
2023-01-30 12:29:24 +0000

    made changes 101

--- qaprojects.py
+++ qaprojects.py
@@ -1,8 +1,17 @@
 import mysql.connector
 
-#Test
-
 input("What is your name?")
 ict_score = input("What was your ICT test score? /100")
 maths_score = input("What was your Maths test score? /100")
 chemistry_score = input("What was your Chemistry test score? name /100")
+
+#fepfke
+mydb = mysql.connector.connect(
+    host= 'localhost',
+    user= 'root',
+    password= 'Pa$$w0rd',
+    database = "testresults"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("CREATE DATABASE testresults")
